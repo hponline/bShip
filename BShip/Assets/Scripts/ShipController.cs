fe1f247df9f464002d30a1dd9c956df3262b0fbf
Apo <abdurrahman_usta_41@hotmail.com>
2025-03-29 20:38:51 +0300

    Environment + BoatController

---
+++ BShip/Assets/Scripts/ShipController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static UnityEditor.PlayerSettings;
+
+public class ShipController : MonoBehaviour
+{
+    public float moveSpeed = 10f;
+    public float laneChangeSpeed; // Şerit değiştirme hızı
+    public int desiredLane = 0; // Şerit 0 ortada demektir.
+    int laneDistance = 53; // Yatay Şerit uzunlugu
+  
+
+    private void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.D))
+        {
+            desiredLane++;            
+        }
+
+        if (Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.A))
+        {
+            desiredLane--;            
+        }
+        desiredLane = Mathf.Clamp(desiredLane, -1, 1);
+        
+        Vector3 targetPosition = transform.position; // Hedef yol
+        targetPosition.x = desiredLane * laneDistance;
+        
+        transform.position = Vector3.Lerp(transform.position, targetPosition, laneChangeSpeed * Time.deltaTime);       
+        transform.position += moveSpeed * Time.deltaTime * -transform.forward;       
+    }
+
+}
