68daf3e7d3cb48b7977678c70e4b8aa67e2d6f8c
Apo <abdurrahman_usta_41@hotmail.com>
2025-03-31 01:38:13 +0300

    Maps ObjectPool

--- BShip/Assets/Scripts/GameManager.cs
+++ BShip/Assets/Scripts/GameManager.cs
@@ -1,10 +1,79 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
     public GameObject[] mapPrefabs;
+    public Transform boat;
+
+    Queue<GameObject> mapPool = new Queue<GameObject>();
+    List<GameObject> activeMaps = new List<GameObject>();
+
+    public int poolSize = 5;
+    float roadLength = 684f;
+    float nextSpawnZ = 0f;
+    public float despawnSpawnZ = 750f;
+
+    private void Awake()
+    {
+        for (int i = 0; i < poolSize; i++) // Mapler oluþturuldu
+        {
+            GameObject obj = Instantiate(mapPrefabs[0], Vector3.zero, Quaternion.identity);
+            obj.SetActive(false);
+            mapPool.Enqueue(obj);
+        }
+
+        for (int i = 0; i < poolSize; i++)
+        {
+            CreateRoad();
+        }
+    }
 
 
+    private void Update()
+    {
+        if (boat.position.z > nextSpawnZ -roadLength)        
+            CreateRoad();
+
+        RecycleOldMap();
+    }
+
+    void CreateRoad() // Map üretme
+    {
+        GameObject mapPart = GetPooledObject();
+        mapPart.transform.position = new Vector3(0, 11.77f, nextSpawnZ);
+        mapPart.SetActive(true);
+
+        nextSpawnZ += roadLength;
+        activeMaps.Add(mapPart);
+    }
+
+    void RecycleOldMap() // Map Geri Dönüþüm
+    {
+        if (activeMaps.Count > 0)
+        {
+            GameObject firstMap = activeMaps[0];
+
+            if (boat.position.z - firstMap.transform.position.z > despawnSpawnZ)
+            {
+                activeMaps.RemoveAt(0);
+                firstMap.SetActive(false);
+                mapPool.Enqueue(firstMap);
+            }
+        }
+    }
+
+    GameObject GetPooledObject() // Havuzdan Map çekme
+    {
+        if (mapPool.Count > 0)
+        {
+            GameObject obj = mapPool.Dequeue();
+            return obj;
+        }
+        else
+        {
+            GameObject obj = Instantiate(mapPrefabs[0]);
+            return obj;
+        }
+    }    
 }
