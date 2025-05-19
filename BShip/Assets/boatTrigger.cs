e9999ee4d9aaa9a6bccd44d1a0874bcb76c31f98
Apo <abdurrahman_usta_41@hotmail.com>
2025-05-19 16:27:26 +0300

    boat animations

---
+++ BShip/Assets/boatTrigger.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class boatTrigger : MonoBehaviour
+{
+    public GameObject boat;
+    public Transform startPos;
+    public Transform endPos;
+    public float boatSpeed;
+    bool deneme = false;
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            deneme = true;
+        }
+    }
+    private void Update()
+    {
+        if (deneme)
+        {
+            boat.transform.position = Vector3.MoveTowards(boat.transform.position, endPos.transform.position, boatSpeed * Time.deltaTime);
+        }
+        if (boat.transform.position == endPos.position)
+        {
+            deneme = false;
+            boat.transform.position = startPos.position;
+        }
+    }
+}
