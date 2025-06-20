9b67fb7c5b8ad67cefc4eaa43d93e39e95524989
Apo <abdurrahman_usta_41@hotmail.com>
2025-06-21 00:58:01 +0300

    SplashEffect

---
+++ BShip/Assets/Scripts/Map/SplashTrigger.cs
@@ -0,0 +1,17 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SplashTrigger : MonoBehaviour
+{
+    public GameObject splashEffekt;
+    HashSet<GameObject> splashedObject = new(); // HashSet sadece o ogeden 1 tane saklar
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!splashedObject.Contains(other.gameObject))
+        {
+            Instantiate(splashEffekt, other.transform.transform.position, Quaternion.identity);
+            splashedObject.Add(other.gameObject);
+        }
+    }
+}
